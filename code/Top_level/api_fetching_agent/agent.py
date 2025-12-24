fc646d08fd9ac10ca197bc0d836d78d08fd087e4
Ahmed Tamer <ahmed.t.samir@gmail.com>
2025-12-24 06:24:48 +0200

    add root_archestrator agent and change folder [code] structure  starting from Top_level directory

--- code/Top_level/api_fetching_agent/agent.py
+++ code/Top_level/api_fetching_agent/agent.py
@@ -3,7 +3,7 @@ from zoneinfo import ZoneInfo
 from google.adk.agents import Agent
 from .prompt import api_fetching_prompt
 from .tools import fetch_weather, fetch_exchange_rate
-root_agent = Agent(
+api_fetching_agent = Agent(
     name="api_fetching_agent",
     model="gemini-2.5-flash",
     description=(
